1519a7b9988143ef4c9114d9b7e355ef7ff73cf4
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-05-16 21:47:38 +0300

    Editing .gitignore

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onload = function () {
 
     var score = 0;
 
-    drawBorder = function () {
+    var drawBorder = function () {
         ctx.fillStyle = 'Gray';
         ctx.fillRect(0, 0, width, blockSize);
         ctx.fillRect(0, height - blockSize, width, blockSize);
@@ -18,5 +18,46 @@ window.onload = function () {
         ctx.fillRect(width - blockSize, 0, blockSize, height);
     };
 
-    drawBorder();
-}
+    var drawScore = function () {
+        ctx.font = '20px Courier';
+        ctx.fillStyle = 'Black';
+        ctx.textAlign = 'left';
+        ctx.textBaseline = 'top';
+        ctx.fillText('Score: ' + score, blockSize, blockSize);
+    };
+
+    var gameOver = function () {
+        //  clearInterval(intervalId);
+        ctx.font = "60px Courier";
+        ctx.fillStyle = "Black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText("Конец игры", width / 2, height / 2);
+    };
+
+    var Block = function (col, row) {
+        this.col = col;
+        this.row = row;
+    };
+
+    Block.prototype.drawSquare = function (color) {
+        var x = this.col * blockSize;
+        var y = this.row * blockSize;
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    };
+
+    Block.prototype.drawCircle = function (color) {
+        var centerX = this.col * blockSize + blockSize / 2;
+        var centerY = this.row * blockSize + blockSize / 2;
+        ctx.fillStyle = color;
+        circle(centerX, centerY, blockSize / 2, true);
+    };
+
+    Block.prototype.equal = function (otherBlock) {
+        return this.col === otherBlock.col && this.row === otherBlock.row;
+    };
+
+
+
+};
